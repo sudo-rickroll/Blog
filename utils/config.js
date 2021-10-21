6ef2006fd99eaeb23cfc17f2d4b686b99110b40b
Rangasai K R <sai.ranga3@gmail.com>
2021-10-22 04:13:48 +0530

    4.1

--- utils/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const PORT = process.env.PORT
-const URL = process.env.MONGODB_URI
-
-module.exports = {
-  PORT,
-  URL
-}
--- utils/logger.js
+++
@@ -1,8 +0,0 @@
-const info = (...inputs) => console.log(...inputs)
-
-const error = (...errors) => console.error(...errors)
-
-module.exports = {
-  info,
-  error
-}
