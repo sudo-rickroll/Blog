faac6bd9be9fb514f7e5569226669d452c5db7e4
Rangasai K R <sai.ranga3@gmail.com>
2021-10-28 16:24:18 +0530

    4.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,10 +7,25 @@ router.get('/', async (request, response) => {
   response.send(blogs)
 })
 
+router.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.send(blog)
+})
+
 router.post('/', async (request, response) => {
   const blog = new Blog(request.body)
   const result = await blog.save()
   response.status(201).send(result)
+})
+
+router.delete('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndDelete(request.params.id)
+  if (blog){
+    return response.status(200).send(blog)
+  }
+  response.status(404).send({
+    error: `Item with ID ${request.params.id} not found`
+  })
 })
 
 module.exports = router
