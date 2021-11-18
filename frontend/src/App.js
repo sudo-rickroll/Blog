c1e749ce27aee7965b48a3ad032042f39bfeb2df
Rangasai K R <sai.ranga3@gmail.com>
2021-11-18 22:46:11 +0530

    5.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Blogs from './components/Blogs'
 import Notifications from './components/Notifications'
-import blogService from './services/blogs'
 import LoginForm from './components/Login'
 import loginService from './services/login'
 
 const App = () => {
-  const [notification, setNotification] = useState({})
-  const [blogs, setBlogs] = useState([])
+  const [notification, setNotification] = useState({})  
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')  
-  const [newBlog, setNewBlog] = useState({})
-  const [loggedIn, setLoggedIn] = useState(window.localStorage.getItem('token') ? true : false)
   const formInputStyle = {
     marginBottom: 5
 }
-  useEffect(() => {
-    if (!blogs.length && loggedIn) {
-      blogService.getAll()
-      .then(data => setBlogs(data))
-      .catch(error => {
-        console.log(error.response.data.error || error.response.data)
-        setNotification({
-          error : error.response.data.error || error.response.data
-        })
-        setTimeout(() => setNotification({}), 5000)
-      })
-    }   
-  }, [blogs, loggedIn])
   
   const handleLogin = async (event) => {
     event.preventDefault()
     try{
       const user = await loginService.validateLogin({username, password})
       window.localStorage.setItem('token', user.token)
+      window.localStorage.setItem('user', user.name)
       setNotification({
         success : `Successfully logged in, user ${user.name}` 
       })
       setTimeout(() => setNotification({}), 5000)
-      setLoggedIn(true)
     }
     catch(error){
       console.log(error.response.data.error || error.response.data)
@@ -52,35 +35,17 @@ const App = () => {
 
   const handleLogout = () => {
     window.localStorage.removeItem('token')
+    window.localStorage.removeItem('user')
     setNotification({
       success : `Successfully logged out`
     })
     setTimeout(() => setNotification({}), 5000)
-    setLoggedIn(false)
   }
 
-  const createNewBlog = async (event) => {
-    event.preventDefault()
-    try{
-      await blogService.createNew(newBlog, window.localStorage.getItem('token'))
-      setNotification({
-        success : `Blog added successfully`
-      })
-      setTimeout(() => setNotification({}), 5000)
-      setBlogs([])
-    }
-    catch(error){
-      console.log(error.response.data.error || error.response.data || error.message)
-      setNotification({
-        error : error.response.data.error || error.response.data || error.message
-      })
-      setTimeout(() => setNotification({}), 5000)
-    }
-  }
   return (
     <div>
       <Notifications message = {notification} />
-      {loggedIn ? <Blogs blogs={blogs} clear={handleLogout} user={username} newBlog={{newBlog, setNewBlog}} handleSubmit={createNewBlog} style={formInputStyle}/> : <LoginForm setUsername = {setUsername} setPassword={setPassword} handleSubmit={handleLogin} style={formInputStyle}/>}
+      {window.localStorage.getItem('token') ? <Blogs clear={handleLogout} notify={setNotification} style={formInputStyle}/> : <LoginForm setUsername = {setUsername} setPassword={setPassword} handleSubmit={handleLogin} style={formInputStyle}/>}
     </div>
   )
 }
--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -1,30 +1,65 @@
 import Blog from "./Blog"
+import React, { useState, useEffect } from 'react'
+import blogService from '../services/blogs'
 
-const Blogs = ({blogs, clear, user, newBlog, handleSubmit, style}) => {
+const Blogs = ({clear, notify, user, style}) => {
+    const [blogs, setBlogs] = useState([])
+    const [newBlog, setNewBlog] = useState({})
+
+    useEffect(() => {
+        if (!blogs.length || !newBlog) {
+          blogService.getAll()
+          .then(data => setBlogs(data))
+          .catch(error => {
+            console.log(error.response.data.error || error.response.data)
+            notify({
+              error : error.response.data.error || error.response.data
+            })
+            setTimeout(() => notify({}), 5000)
+          })
+        }   
+    })
+    
+    const createNewBlog = async (event) => {
+        event.preventDefault()
+        try{
+          await blogService.createNew(newBlog, window.localStorage.getItem('token'))
+          notify({
+            success : `Blog added successfully`
+          })
+          setTimeout(() => notify({}), 5000)
+          setNewBlog(null)
+        }
+        catch(error){
+          console.log(error.response.data.error || error.response.data || error.message)
+          notify({
+            error : error.response.data.error || error.response.data || error.message
+          })
+          setTimeout(() => notify({}), 5000)
+        }
+    }
     const createBlog = (item, value) => {
         const newObject = {}
         newObject[item] = value
-        newBlog.setNewBlog({...newBlog.newBlog, ...newObject})
+        setNewBlog({...newBlog, ...newObject})
     }
-    if (blogs.length){
-        return (
-            <div>
-                <h2>blogs</h2>
-                <p>{user} has logged in <button onClick={clear}>logout</button></p>
-                <h2>create new</h2>
-                <form onSubmit={event => handleSubmit(event)}>
-                    <div style={style}>title <input type='text' onChange={event => {createBlog('title', event.target.value)}}></input></div>
-                    <div style={style}>author <input type='text' onChange={event => {createBlog('author', event.target.value)}}></input></div>
-                    <div style={style}>url <input type='text' onChange={event => {createBlog('url', event.target.value)}}></input></div>
-                    <div style={style}><button type='submit'>create</button></div>
-                </form>
-                {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} />
-                )}
-            </div>
-        )
-    }
-    return null    
+
+    return (
+        <div>
+            <h2>blogs</h2>
+            <p>{window.localStorage.getItem('user')} has logged in <button onClick={clear}>logout</button></p>
+            <h2>create new</h2>
+            <form onSubmit={event => createNewBlog(event)}>
+                <div style={style}>title <input type='text' onChange={event => {createBlog('title', event.target.value)}}></input></div>
+                <div style={style}>author <input type='text' onChange={event => {createBlog('author', event.target.value)}}></input></div>
+                <div style={style}>url <input type='text' onChange={event => {createBlog('url', event.target.value)}}></input></div>
+                <div style={style}><button type='submit'>create</button></div>
+            </form>
+            {blogs ? blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+            ) : null}
+        </div>
+    )   
 }
 
 export default Blogs
