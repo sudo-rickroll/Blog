92136a3c296ef9042b471e1b998af0a55e5f5cd4
Rangasai K R <sai.ranga3@gmail.com>
2021-12-03 22:57:14 +0530

    5.7

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [ visible, setVisible ] = useState(false)
+  const toggleVisibility = () => setVisible(!visible)
+  const displayStyle = {display: visible ? "" : "none"}
+  const buttonLabel = visible ? "hide" : "show"
+
+  return (
+  <div style={blogStyle}>
+    {blog.title} {blog.author} <input type="button" value={buttonLabel} onClick={toggleVisibility}/>
+    <div style={displayStyle} >
+      {blog.url}<br />
+      {blog.likes}<input type="button" value="like" /><br />
+      {blog.user.username}<br/>
+    </div>
+  </div> 
+  )
+
+}
+   
+
 
 export default Blog
